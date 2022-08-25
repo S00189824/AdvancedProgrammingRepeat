c560c1b3c3b9d28f222d5e7b85a99732c47c44e1
David Coleman <46888854+S00189824@users.noreply.github.com>
2022-08-25 23:15:35 +0100

    dfsdf

---
+++ AdvancedProgrammingRepeatExam/IsolatedStorage.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AdvancedProgrammingRepeatExam
+{
+    internal class IsolatedStorage
+    {
+        //create an object to be used by lock()
+        public static Object synObj = new Object();
+        private IsolatedStorageFile store;
+
+        //the name of the folder we will create in each isolated store
+        private string folderName;
+
+        //the path to the file - we will create a text file for each isolated store
+        private string pathToTextFile;
+
+        //constructor method which creates the isolated storage
+        public IsolatedStorage(int selIndex)
+        {
+            //give the folder a name
+            folderName = "ColourFolder";
+            //set the path to the text file
+            pathToTextFile = String.Format("{0}\\____File.txt", folderName);
+
+            //set the isolation storage type and access the store (obtain the isolated store)
+            if (selIndex == 0)
+                store = IsolatedStorageFile.GetUserStoreForDomain();
+            else
+                store = IsolatedStorageFile.GetUserStoreForAssembly();
+        }
+
+        //method which writes a colour to ColourFile.txt in the selected Isolated store 
+        public void writeToStorage(Object colourFromUser)
+        {
+            string colourToSaveToStorage = ____FromUser.ToString();
+
+            //check if the isolated store was obtained successfully (the code for that is in the constructor method)
+            if (store != null)
+            {
+
+                //synchronise access to the isolated storage text file 
+                Monitor.Enter(synObj);
+                try
+                {
+                    //check if the folder exists.  If it does not, then create it
+                    if (!store.DirectoryExists(folderName))
+                        store.CreateDirectory(folderName);
+
+                    //create the isolated storage file (the text file ColourFile.txt)
+                    //We create a new ColourFile.txt every time this method is called
+                    //- the new text file will overwrite any existing text file 
+                    //(notice the FileMode.Create in the code below)
+                    using (IsolatedStorageFileStream isoStorageTxtFile =
+                        store.OpenFile(pathToTextFile, FileMode.Create, FileAccess.Write))
+                    {
+                        using (StreamWriter writer = new StreamWriter(isoStorageTxtFile))
+                        {
+                            writer.Write(colourToSaveToStorage);
+                            MessageBox.Show("All good - colour saved to ColourFile.txt");
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+                finally
+                {
+                    Monitor.Pulse(synObj);
+                    Monitor.Wait(synObj);
+                }
+            }
+
+        }
+
+        //method which reads the colour from ColourFile.txt (which is placed in the selected Isolated store) 
+        public void readFromStorage()
+        {
+            if (store != null)
+            {
+                Monitor.Enter(synObj);
+                try
+                {
+                    //read from the text file ColourFile.txt
+                    using (IsolatedStorageFileStream isoStorageTxtFile = store.OpenFile(pathToTextFile, FileMode.Open, FileAccess.Read))
+                    {
+                        using (StreamReader reader = new StreamReader(isoStorageTxtFile))
+                        {
+                            string colourFromTextFile = reader.ReadLine();
+
+                            //we have to apply the colour as the background of the main Window
+                            // we use Dispatcher.Invoke in order to accesss the controls on the main Window
+                            // we need to point to an instance of the MainWindow class and then call Dispatcher.Invoke() on it
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+                finally
+                {
+                    Monitor.Pulse(synObj);
+                    Monitor.Wait(synObj);
+                }
+            }
+        }
+    }
+}
---
+++ AdvancedProgrammingRepeatExam/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdvancedProgrammingRepeatExam
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BackgroundWorker backgroundWorker = new BackgroundWorker();
+
+        public string[] wordArray = new string[3] { "sdfsd", "sdfsd", "edfe" };
+        public int[] intArray = new int[3];
+        const int secondsToWait = 3000; // Thread Timer
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            backgroundWorker.DoWork += BackgroundWorker_DoWork;
+            backgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
+            backgroundWorker.ProgressChanged += BackgroundWorker_ProgressChanged;
+
+            backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.WorkerSupportsCancellation = true;
+        }
+
+        private void BackgroundWorker_ProgressChanged(object? sender, ProgressChangedEventArgs e)
+        {
+            // Displays progress bar progress of background worker
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void BackgroundWorker_RunWorkerCompleted(object? sender, RunWorkerCompletedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BackgroundWorker_DoWork(object? sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (backgroundWorker.CancellationPending)// to check if cancellation is pending and then cancels
+                {
+                    e.Cancel = true;
+                    return;
+                }
+
+                MessageBox.Show("");
+
+
+            }
+        }
+
+        private void btn_StartClick(object sender, RoutedEventArgs e)
+        {
+            //Starts execution of backgroundworker
+            backgroundWorker.RunWorkerAsync();
+        }
+    }
+}
